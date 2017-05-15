190a50ae50bd243b6240b65d9dd95e394cffff46
Fahad Qazi <fahadqazi@gmail.com>
2017-05-15 23:21:47 +0100

    partial currying

--- curryingNew.js
+++ curryingNew.js
@@ -2,3 +2,16 @@
 // You can call a function with fewer arguments than it expects, by
 // returning a function and passing the remaining arguments to that function.
 
+// Creating a partial function using ES6 syntax
+
+var partialFromBind = (fn, ...args) => {
+    return fn.bind(null, ...args);
+}
+
+// Can be created manually
+
+var partial = (fn, ...args) => {
+    return (...otherArgs) => {
+        return fn(...args, ...otherArgs);
+    }
+}
