b1bb12ec7fb6e1c6b422deccfbc2b1505cffe304
Fahad Qazi <fahadqazi@gmail.com>
2017-05-16 01:42:12 +0100

    examples of currying

--- curryingNew.js
+++ curryingNew.js
@@ -31,3 +31,44 @@ var adder = function(x){
 
 var result = adder(1)(2)(3);
 console.log(result);
+
+// ES6 way
+
+var adder1 = x => y => z => x + y + z;
+var result1 = adder1(1)(2)(3);
+console.log(result1);
+
+// Example
+var double = x => x*2;
+
+var map1 = fn => array => array.map(fn);
+
+var res1 = map1(double)([1,2,3,4,5]);
+console.log(res1);
+
+//OR
+
+var map2 = function(fn){
+    return function(array){
+        return array.map(fn);
+    }
+}
+
+// Large example
+
+var map = fn => array => array.map(fn);
+var multiply = x => y => x * y;
+var pluck = key => object => object[key];
+
+var discount = multiply(0.98);
+var tax = multiply(1.0925);
+
+var obj = [
+    {price: 5},
+    {price: 10},
+    {price: 15}
+];
+
+var newR = obj.map(pluck('price')).map(discount).map(tax);
+console.log(newR);
+
