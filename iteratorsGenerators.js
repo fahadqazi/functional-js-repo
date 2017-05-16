ceaa3dd588fa5e2ab4fa619e028f9ff9c405be14
Fahad Qazi <fahadqazi@gmail.com>
2017-05-16 22:48:43 +0100

    version 2

--- iteratorsGenerators.js
+++ iteratorsGenerators.js
@@ -27,6 +27,12 @@
 
 // version 2 - now testing team becomes part of the engineering team.
 
+var testingTeam = {
+    manager: 'Joe',
+    tester: 'Kevin'
+}
+
+
 var engineeringTeam = {
     testingTeam: testingTeam,
     size: 3,
@@ -36,8 +42,21 @@ var engineeringTeam = {
     engineer: 'Bob'
 }
 
-var testingTeam = {
-    manager: 'Joe',
-    tester: 'Kevin'
+function* engineeringTeamIterator(team) {
+    yield team.manager;
+    yield team.engineer;
+    yield team.lead;
+    var testTeam = testTeamIterator(team.testingTeam);
+    yield* testTeam;
 }
 
+function* testTeamIterator(team){
+    yield team.manager;
+    yield team.tester;
+}
+
+var newArr2 = [];
+for(let val of engineeringTeamIterator(engineeringTeam)){
+    newArr2.push(val)
+}
+console.log(newArr2);
